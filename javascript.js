c169c57c4a79fd79d1fa753a44b4075408e43524
Jimmy <jimmy.hotchkiss@gmail.com>
2021-12-23 08:59:10 -0800

    Added hidden menu animation.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,35 @@
 const coffeeMenu = document.getElementById('coffee-menu')
 const coffeeBtn = document.getElementById('coffee-btn')
 
-function menuToggle() {
-    if (coffeeMenu.style.display === 'block'){
-        coffeeMenu.style.display = 'none'
+// function menuToggle() {
+//     if (coffeeMenu.classList.contains('hidden')){
+//         coffeeMenu.classList.remove('hidden')
+//         coffeeMenu.classList.add('show')
+//     } else {
+//         coffeeMenu.classList.remove('show')
+//         coffeeMenu.classList.add('hidden')
+//     }
+// }
+
+function openMenu() {
+    if (coffeeMenu.classList.contains('show')){
+        coffeeMenu.classList.remove('show')
+        coffeeMenu.classList.add('hidden')
     } else {
-        coffeeMenu.style.display = 'block'
+        coffeeMenu.classList.remove('hidden')
+        coffeeMenu.classList.add('show')
     }
-    // coffeeMenu.classList.toggle('show')
 }
 
-coffeeBtn.addEventListener('click', menuToggle)
+function closeMenu() {
+    if (coffeeMenu.classList.contains('hidden')){
+        coffeeMenu.classList.remove('hidden')
+        coffeeMenu.classList.add('show')
+    } else {
+        coffeeMenu.classList.remove('show')
+        coffeeMenu.classList.add('hidden')
+    }
+}
+
+coffeeBtn.addEventListener('mouseover', menuToggle)
 
